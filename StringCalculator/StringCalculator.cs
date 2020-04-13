e34fa2ea0a2655b96c5d8c1637e3f8279d2bcb5c
Sahil Muniram <Sahil@chilli.local>
2020-04-13 23:09:47 +0200

    StringCalculator Project Initial Commit.

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            string[] input = numbers.Split(new char[]{ ',', '\n'});
+            int totalNumbers = input.Length;
+            
+            if (totalNumbers == 1)
+                return Convert.ToInt32(input[0]);
+
+            if (totalNumbers == 2)
+                return Convert.ToInt32(input[0]) + Convert.ToInt32(input[1]);
+
+            if (totalNumbers > 2)
+            {
+                int sum = 0;
+                foreach (var num in input)
+                {
+                    sum += Convert.ToInt32(num);
+                }
+                return sum;
+            }
+            
+            return 0;
+        }
+    }
+}
---
+++ TestStringCalculator/TestStringCalculator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    public class TestStringCalculator
+    {
+        readonly StringCalculator _stringCalculator = new StringCalculator();
+        
+        [Test]
+        public void Add_WhenEmptyString_Return0()
+        {
+            //Arrange 
+            const string input = "";
+            const int expectedOutput = 0;
+            int output;
+
+            //Act 
+            output = _stringCalculator.Add(input);
+
+            //Assert
+            Assert.AreEqual(expectedOutput,output);
+        }
+
+        [Test]
+        public void Add_WhenInputHas1Number_ReturnInput()
+        {
+            //Arrange 
+            const string input = "2";
+            const int expectedOutput = 2;
+            int output;
+
+            //Act 
+            output = _stringCalculator.Add(input);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void Add_WhenInputHas2Numbers_ReturnSum()
+        {
+            //Arrange
+            const string input = "2,3";
+            const int expectedOutput = 5;
+            int output;
+
+            //Act
+            output = _stringCalculator.Add(input);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void Add_WhenInputHasMultipleNumbers_ReturnSum()
+        {
+            //Arrange
+            const string input = "2,3,5";
+            const int expectedOutput = 10;
+            int output;
+            
+            //Act
+            output = _stringCalculator.Add(input);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void Add_WhenInputHasNewLineDelimiter_ReturnSum()
+        {
+            //Arrange
+            const string input = "1\n2,3";
+            const int expectedOutput = 6;
+            int output;
+
+            //Act
+            output = _stringCalculator.Add(input);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+    }
+}
